894bca80f4ada377555e7af1567445bf8ab74e5a
melaniefitzmaurice <melaniefitzmaurice@gmail.com>
2018-07-12 16:31:18 +1000

    Finished class

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,1 +1,47 @@
 cssVars();
+
+$('.nav-prompt').click(function () {
+   
+    if ($('.nav-inner').attr('aria-expanded') == 'false') {
+        $('.nav-inner').attr('aria-expanded', 'true');
+    }
+    else {
+        $('.nav-inner').attr('aria-expanded', 'false');
+
+    };
+
+    if ($('.nav-prompt').text () == 'Open Navigation') {
+        $('.nav-prompt').text('Close Navigation');
+    }
+    else {
+        $('.nav-prompt').text('Open Navigation');
+    }
+
+});
+
+/* nav active class */
+
+function activeMenu() {
+    var url = window.location.href //the page we are on//
+
+    $('.nav-inner a').filter( function() {
+        //this .href is looking through the nav items//
+        return this.href == url;
+    }).addClass('active');
+}
+
+activeMenu();
+
+
+/*image gallery*/
+
+function imageGallery() {
+    if (!$('.image-gallery').length) {
+        return;
+    }
+
+    $('.image-gallery a').simpleLightbox();
+}
+
+imageGallery();
+
